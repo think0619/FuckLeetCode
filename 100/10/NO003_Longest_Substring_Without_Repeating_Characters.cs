e547c4714069b6bc83a926de1c072021f38237fc
think <thinkxs@outlook.com>
2020-04-29 21:46:36 +0800

    split

---
+++ 100/10/NO003_Longest_Substring_Without_Repeating_Characters.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuckingLeetCode
+{
+    class NO003_Longest_Substring_Without_Repeating_Characters
+    {
+        
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            int maxLength = 0;
+            if (s.Length == 0 || s.Length == 1)
+            {
+                maxLength = s.Length;
+            }
+            else
+            { 
+                Dictionary<char, int> charLastIndex = new Dictionary<char, int>();
+                int firstRepeatIndex = -1;
+                int lastRepeatIndex = -1;
+               // char[] sarray = new char[s.Length];
+                int index = 0;
+                foreach (char item in s)
+                {
+                   // sarray[index] = item;
+                    if (charLastIndex.ContainsKey(item))
+                    {
+                        if (firstRepeatIndex == -1)
+                        {
+                            firstRepeatIndex = index;
+                        }
+                        lastRepeatIndex = charLastIndex[item];
+                        //找中间
+                        int lengthTmp = index - charLastIndex[item];
+                        if (maxLength < lengthTmp)
+                        {
+                            maxLength = lengthTmp;
+                        }
+
+                        //  charLastIndex[item] = index;
+                        charLastIndex.Clear();
+
+                    }
+                    charLastIndex.Add(item, index);
+                    //else
+                    //{
+                    //    charLastIndex.Add(item, index);
+                    //}
+                    index++;
+                }
+                if (firstRepeatIndex != -1)
+                {
+                    if (firstRepeatIndex  > maxLength)
+                    {
+                        maxLength = firstRepeatIndex ;
+                    }
+                }
+
+                if (lastRepeatIndex != -1)
+                {
+                    if ((s.Length-lastRepeatIndex-1) > maxLength)
+                    {
+                        maxLength = s.Length - lastRepeatIndex - 1;
+                    }
+                }
+
+                if (firstRepeatIndex == -1 && lastRepeatIndex == -1)
+                {
+                    maxLength = s.Length;
+                }
+            }
+            return maxLength;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,42 +10,45 @@ namespace FuckingLeetCode
     {
         static void Main(string[] args)
         {
-            #region NO.001 
-            NO001_AddTwoNumbers addTwoNumbers = new NO001_AddTwoNumbers();
-            addTwoNumbers.result = addTwoNumbers.TwoSum(addTwoNumbers.TestValue, addTwoNumbers.target);
-            #endregion
 
-            #region NO.015 3Sum
-            NO015_3Sum N3Sum = new NO015_3Sum();
-            N3Sum.result = N3Sum.ThreeSum(N3Sum.TestValue);
-            #endregion
+            NO003_Longest_Substring_Without_Repeating_Characters n = new NO003_Longest_Substring_Without_Repeating_Characters();
+           var x= n.LengthOfLongestSubstring("abba");
+           // #region NO.001 
+           // NO001_AddTwoNumbers addTwoNumbers = new NO001_AddTwoNumbers();
+           // addTwoNumbers.result = addTwoNumbers.TwoSum(addTwoNumbers.TestValue, addTwoNumbers.target);
+           // #endregion
 
-            #region NO.018 4.Sum
-            NO018_4Sum no018_4Sum= new NO018_4Sum();
-            var x= no018_4Sum.FourSum(no018_4Sum.TestValue, no018_4Sum.TestTarget);
-           
-            #endregion
+            // #region NO.015 3Sum
+            // NO015_3Sum N3Sum = new NO015_3Sum();
+            // N3Sum.result = N3Sum.ThreeSum(N3Sum.TestValue);
+            // #endregion
 
-            #region NO.067
-            NO067_AddBinary addBinary = new NO067_AddBinary();
-            addBinary.result = addBinary.AddBinary(addBinary.stringA, addBinary.stringB);
-            #endregion
+            // #region NO.018 4.Sum
+            // NO018_4Sum no018_4Sum= new NO018_4Sum();
+            // var x= no018_4Sum.FourSum(no018_4Sum.TestValue, no018_4Sum.TestTarget);
 
-            #region NO.447 
-            NO447_NumberOfBoomerangs numberOfBoomerangs = new NO447_NumberOfBoomerangs();
-            numberOfBoomerangs.result = numberOfBoomerangs.NumberOfBoomerangs(numberOfBoomerangs.testPoints);
-            #endregion
+            // #endregion
 
-            int[][] testPoints = new int[1][];
-            testPoints[0] = new int[] { -4, -5 }; 
-            //testPoints[0] = new int[] { 3, 0, 1, 4, 2 };
-            //testPoints[1] = new int[] { 5, 6, 3, 2, 1 };
-            //testPoints[2] = new int[] { 1, 2, 0, 1, 5 };
-            //testPoints[3] = new int[] { 4, 1, 0, 1, 7 };
-            //testPoints[4] = new int[] { 1, 0, 3, 0, 5 };
+            // #region NO.067
+            // NO067_AddBinary addBinary = new NO067_AddBinary();
+            // addBinary.result = addBinary.AddBinary(addBinary.stringA, addBinary.stringB);
+            // #endregion
 
-            NumMatrix nm = new NumMatrix(testPoints);
-           var t1= nm.SumRegion( 0,0,0,0 );
+            // #region NO.447 
+            // NO447_NumberOfBoomerangs numberOfBoomerangs = new NO447_NumberOfBoomerangs();
+            // numberOfBoomerangs.result = numberOfBoomerangs.NumberOfBoomerangs(numberOfBoomerangs.testPoints);
+            // #endregion
+
+            // int[][] testPoints = new int[1][];
+            // testPoints[0] = new int[] { -4, -5 }; 
+            // //testPoints[0] = new int[] { 3, 0, 1, 4, 2 };
+            // //testPoints[1] = new int[] { 5, 6, 3, 2, 1 };
+            // //testPoints[2] = new int[] { 1, 2, 0, 1, 5 };
+            // //testPoints[3] = new int[] { 4, 1, 0, 1, 7 };
+            // //testPoints[4] = new int[] { 1, 0, 3, 0, 5 };
+
+            // NumMatrix nm = new NumMatrix(testPoints);
+            //var t1= nm.SumRegion( 0,0,0,0 );
         }
     }
 }
